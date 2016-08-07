186c9ee2dcf2d0a546dbc2a56fece1cb665e1b76
Mira <szewczyk.mira@gmail.com>
2016-08-07 13:10:54 +0200

    Two assigments done

--- Week1/recfun/src/main/scala/recfun/Main.scala
+++ Week1/recfun/src/main/scala/recfun/Main.scala
@@ -12,13 +12,33 @@ object Main {
 
   /**
    * Exercise 1
+    That is (row column)
    */
-    def pascal(c: Int, r: Int): Int = ???
+    def pascal(column: Int, row: Int): Int = {
+      def factor(acc: Int, elem:Int) : Int = 
+        if (elem == 1) acc else factor(acc*elem, elem-1)
+      
+      if (column == 0 || column == row) 1 else
+        factor(1, row) / (factor(1, column) * factor(1, row-column))
+  }
   
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
+    def balance(chars: List[Char]): Boolean = {
+      def charToInt(char: Char): Int =  char match {
+        case '(' => 1
+        case ')' => -1
+        case _ => 0
+      }
+      
+      def parse(acc: Int, chars: List[Char]) : Int = {
+        if(chars.isEmpty) acc else
+          if(acc < 0) -1 else parse(acc + charToInt(chars.head), chars.tail)
+      }
+      
+      if(parse(0, chars) == 0) true else false 
+  }
   
   /**
    * Exercise 3
